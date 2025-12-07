cdb5be6d9ab9053dc48eee9df7247df56748075f
HectorGaraboa <hectorgaraboa77@gmail.com>
2025-12-07 22:36:27 +0100

    no message

--- back/tpv-api/src/main/java/com/hector/tpv/tpvapi/service/ReportService.java
+++ back/tpv-api/src/main/java/com/hector/tpv/tpvapi/service/ReportService.java
@@ -21,77 +21,76 @@ public class ReportService {
         this.dataSource = dataSource;
     }
 
-   public String generarTicketPdf(Long ticketId) {
+    public String generarTicketPdf(Long ticketId) {
 
-    try (Connection connection = dataSource.getConnection()) {
-        InputStream jasperStream =
-                getClass().getResourceAsStream("/reports/Ticket.jasper");
+        try (Connection connection = dataSource.getConnection()) {
+            InputStream jasperStream
+                    = getClass().getResourceAsStream("/reports/Ticket.jasper");
 
-        if (jasperStream == null) {
-            throw new RuntimeException("No se encuentra Ticket.jasper");
+            if (jasperStream == null) {
+                throw new RuntimeException("No se encuentra Ticket.jasper");
+            }
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("TICKET_ID", ticketId);
+
+            JasperPrint print = JasperFillManager.fillReport(
+                    jasperStream,
+                    params,
+                    connection
+            );
+
+            Path carpeta = Paths.get("tickets");
+            if (!Files.exists(carpeta)) {
+                Files.createDirectories(carpeta);
+            }
+
+            String nombreArchivo = "ticket_" + ticketId + ".pdf";
+            Path rutaPdf = carpeta.resolve(nombreArchivo);
+
+            JasperExportManager.exportReportToPdfFile(print, rutaPdf.toString());
+
+            return rutaPdf.toAbsolutePath().toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error generando PDF de ticket " + ticketId, e);
         }
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("TICKET_ID", ticketId);
-
-        JasperPrint print = JasperFillManager.fillReport(
-                jasperStream,
-                params,
-                connection
-        );
-
-        Path carpeta = Paths.get("tickets");
-        if (!Files.exists(carpeta)) {
-            Files.createDirectories(carpeta);
-        }
-
-        String nombreArchivo = "ticket_" + ticketId + ".pdf";
-        Path rutaPdf = carpeta.resolve(nombreArchivo);
-
-        JasperExportManager.exportReportToPdfFile(print, rutaPdf.toString());
-
-        return rutaPdf.toAbsolutePath().toString();
-
-    } catch (Exception e) {
-        throw new RuntimeException("Error generando PDF de ticket " + ticketId, e);
     }
-}
 
-       public String generarCierreTurnoPdf(Long cierreId) {
+    public String generarCierreTurnoPdf(Long cierreId) {
 
-    try (Connection connection = dataSource.getConnection()) {
-        InputStream jasperStream =
-                getClass().getResourceAsStream("/reports/CierreTurno.jasper");
+        try (Connection connection = dataSource.getConnection()) {
+            InputStream jasperStream
+                    = getClass().getResourceAsStream("/reports/CierreTurno.jasper");
 
-        if (jasperStream == null) {
-            throw new RuntimeException("No se encuentra CierreTurno.jasper");
+            if (jasperStream == null) {
+                throw new RuntimeException("No se encuentra CierreTurno.jasper");
+            }
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("CIERRE_ID", cierreId);
+
+            JasperPrint print = JasperFillManager.fillReport(
+                    jasperStream,
+                    params,
+                    connection
+            );
+
+            Path carpeta = Paths.get("cierres");
+            if (!Files.exists(carpeta)) {
+                Files.createDirectories(carpeta);
+            }
+
+            String nombreArchivo = "cierre_" + cierreId + ".pdf";
+            Path rutaPdf = carpeta.resolve(nombreArchivo);
+
+            JasperExportManager.exportReportToPdfFile(print, rutaPdf.toString());
+
+            return rutaPdf.toAbsolutePath().toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error generando PDF de cierre " + cierreId, e);
         }
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("CIERRE_ID", cierreId);
-
-        JasperPrint print = JasperFillManager.fillReport(
-                jasperStream,
-                params,
-                connection
-        );
-
-        Path carpeta = Paths.get("cierres");
-        if (!Files.exists(carpeta)) {
-            Files.createDirectories(carpeta);
-        }
-
-        String nombreArchivo = "cierre_" + cierreId + ".pdf";
-        Path rutaPdf = carpeta.resolve(nombreArchivo);
-
-        JasperExportManager.exportReportToPdfFile(print, rutaPdf.toString());
-
-        return rutaPdf.toAbsolutePath().toString();
-
-    } catch (Exception e) {
-        throw new RuntimeException("Error generando PDF de cierre " + cierreId, e);
     }
-}
-
 
 }
